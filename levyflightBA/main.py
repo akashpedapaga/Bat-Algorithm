661ea1a8c4e76e5a90b9b5c0e1122e70e32ddf00
KazaGeetanjali <gk563@scarletmail.rutgers.edu>
2024-04-22 15:09:02 -0400

    Levy flight initial commit

---
+++ levyflightBA/main.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+import numpy as np
+
+import plot
+import testfunctions
+from levybat import Bat
+
+
+def run(pca, function, lb, ub, generations):
+    alpha_gamma = 0.95
+    algorithm = Bat(
+        # Dimension
+        100,
+        # Population
+        100,
+        # Generations
+        generations,
+        # Loudness
+        0.9,
+        # Pulse rate
+        0.9,
+        # Min. Freq.
+        0.0,
+        # Max. Freq.
+        5.0,
+        # Lower bound
+        lb,
+        # Upper bound
+        ub,
+        function,
+        alpha=0.99,
+        gamma=0.9,
+        use_pca=pca,
+        levy=True,
+    )
+    return_dict = algorithm.run_bats()
+    print(
+        f"Best: {return_dict['best']}, values: {return_dict['final_fitness']}"
+    )
+    return return_dict
+
+
+def main():
+    # Define a run here:
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ levyflightBA/testfunctions.py
@@ -0,0 +1,82 @@
+import numpy as np
+import scipy
+
+
+def willem(x):
+    return x / x / x / x
+
+
+# Rosenbrock test function
+def FRosenbrock(x):
+    return scipy.optimize.rosen(x)
+
+
+def rosen(x):
+    """The Rosenbrock function"""
+    return sum(100.0 * (x[1:] - x[:-1] ** 2.0) ** 2.0 + (1 - x[:-1]) ** 2.0)
+
+
+def stub(x):
+    value = 0.0
+    for i in range(x.shape[0]):
+        value += np.sum((-x[i] * np.sin(np.sqrt(np.abs(x[i])))))
+    return value
+
+
+# Sphere test function
+def FSphere(x):
+    """
+    Sphere function
+    range: [np.NINF, np.inf]
+    """
+    return (x**2).sum()
+
+
+# Rastrigin test function
+def FRastrigin(x):
+    """
+    Rastrigin's function
+    [-5.12, 5.12]
+    """
+    return np.sum(x**2 - 10.0 * np.cos(2.0 * np.pi * x) + 10)
+
+
+# Griewank test function
+def FGrienwank(x):
+    """Griewank's function"""
+    i = np.sqrt(np.arange(x.shape[0]) + 1.0)
+    return np.sum(x**2) / 4000.0 - np.prod(np.cos(x / i)) + 1.0
+
+
+# Weierstrass test function
+def FWeierstrass(x):
+    """Weierstrass's function"""
+    alpha = 0.5
+    beta = 3.0
+    kmax = 20
+    D = x.shape[0]
+    exprf = 0.0
+
+    c1 = alpha ** np.arange(kmax + 1)
+    c2 = 2.0 * np.pi * beta ** np.arange(kmax + 1)
+    f = 0
+    c = -D * np.sum(c1 * np.cos(c2 * 0.5))
+
+    for i in range(D):
+        f += np.sum(c1 * np.cos(c2 * (x[i] + 0.5)))
+    return f + c
+
+
+def F8F2(x):
+    f2 = 100.0 * (x[0] ** 2 - x[1]) ** 2 + (1.0 - x[0]) ** 2
+    return 1.0 + (f2**2) / 4000.0 - np.cos(f2)
+
+
+# FEF8F2 function
+def FEF8F2(x):
+    D = x.shape[0]
+    f = 0
+    for i in range(D - 1):
+        f += F8F2(x[[i, i + 1]] + 1)
+    f += F8F2(x[[D - 1, 0]] + 1)
+    return f
